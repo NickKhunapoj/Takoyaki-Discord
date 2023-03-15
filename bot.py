aa3934f94e23f4af7759ec27ef9d9ceece3eb5a6
Nick GOGOGO <93870412+NickKhunapoj@users.noreply.github.com>
2023-03-15 08:49:33 +0700

    updated code

--- bot.py
+++ bot.py
@@ -1,40 +1,54 @@
 import discord
 import os
 import responses
+import azure.cognitiveservices.speech as speechsdk
 
+''' Core bot code '''
+
+# Set up tokens
+bot_token = os.environ['takoyakibot_key']
+tts_token = os.environ['tts_key']
+# Set up client
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+# Set up speech config
+speech_config = speechsdk.SpeechConfig(subscription=tts_token, region="japaneast")
+speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+
+# Set up message sending function
 async def send_message(message, user_message, is_private):
     try:
+        global response
         response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
-    
+
+'''Run the bot'''
+
 def run_discord_bot():
-    bot_token = os.environ['takoyakibot_key']
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
 
+    
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
-        
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
         print (f'{username} sent a message in {channel}: {user_message}')
 
-        if user_message[0] == '?':
+        if user_message.startswith('?'):
             user_message = user_message[1:]
-            await send_message(message, user_message, is_private = True)
+            await send_message(message, user_message, is_private=True)
         else:
-            await send_message(message, user_message, is_private = False)
+            await send_message(message, user_message, is_private=False)
 
 
     client.run(bot_token)
--- responses.py
+++ responses.py
@@ -8,16 +8,44 @@ def handle_response(conversation):
     openai.api_key = api_key
     model_id = 'gpt-3.5-turbo'
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": conversation},
-        ]
-    )
-    # Print the response
-    response_json = str(response)
-    response_dict = json.loads(response_json)
-    content = response_dict['choices'][0]['message']['content']
-    clean_content = content.replace('\n\n', '\n')
+    try:
+        response = openai.ChatCompletion.create(
+            model=model_id,
+            messages=[
+                {"role": "assistant", "content": conversation},
+            ]
+        )
+        # Print the response
+        response_json = str(response)
+        response_dict = json.loads(response_json)
+        content = response_dict['choices'][0]['message']['content']
+        clean_content = content.replace('\n\n', '\n')
+        return clean_content
+    
+    except Exception as e:
+        print(f"Error calling OpenAI API: {e}")
+        return "Sorry, I'm having trouble understanding you right now."
 
-    return clean_content
+'''class Chat:
+    def __init__(self):
+        # Setting the API key to use the OpenAI API
+        api_key = os.environ['openai_key']
+        openai.api_key = api_key
+        self.messages = [
+            {"role": "system", "content": "You are a general assistant bot to provide information to users."},
+        ]
+
+    def handle_response(self, conversation):
+        self.messages.append({"role": "user", "content": conversation})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages
+        )
+        self.messages.append({"role": "assistant", "content": response["choices"][0]["message"].content})
+
+        # Print the response
+        response_json = str(response)
+        response_dict = json.loads(response_json)
+        content = response_dict['choices'][0]['message']['content']
+        clean_content = content.replace('', '')
+        return clean_content'''
